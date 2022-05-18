dfd9460cf60d71b3ab5ac1178b0639cc90580975
Afraz <79227256+Afrazsheikh@users.noreply.github.com>
2022-05-18 12:42:59 +0530

    Update c1.sol

--- c1.sol
+++ c1.sol
@@ -17,7 +17,7 @@ abstract contract ERC20Token {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 }
 
-contract Owned {
+contract Owned { //this define something to be owned 
     address public owner;
     address public newOwner;
 
@@ -41,24 +41,25 @@ contract Owned {
 }
 
 contract Token is ERC20Token, Owned { //this means every  contarct token is type of contact ERC20token and owned 
+//simple inhertamce from  the ERC20token
  
-    string public _symbol;
-    string public _name;
-    uint8 public _decimal;
-    uint public _totalSupply;
-    address public _minter;
+    string public _symbol; // decide symbol for the contract
+    string public _name; // name for the contract
+    uint8 public _decimal;   // for decimal value
+    uint public _totalSupply; // total supply of the contrct
+    address public _minter; //for minting the token
 
-    mapping(address => uint) balances;
+    mapping(address => uint) balances;  // here the mapping of the adress help to keeping the record of the transaction
 
     constructor () {
         _symbol = "Tk";
         _name = "Token";
         _decimal = 0;
         _totalSupply = 100;
-        _minter =  0xcE6E504A905A3616a14f6E93c6A426d312023F34;
+        _minter =  0xcE6E504A905A3616a14f6E93c6A426d312023F34;  
 
-        balances[_minter] = _totalSupply;
-        emit Transfer(address(0), _minter, _totalSupply);
+        balances[_minter] = _totalSupply;  // this gives the inital supply to the address which minter is store
+        emit Transfer(address(0), _minter, _totalSupply);   // transfer event  event Transfer(address indexed _from, address indexed _to, uint256 _value);     
     }
 
     function name() public override view returns (string memory) {
@@ -78,13 +79,13 @@ contract Token is ERC20Token, Owned { //this means every  contarct token is type
     }
 
     function balanceOf(address _owner) public override view returns (uint256 balance) {
-        return balances[_owner];
+        return balances[_owner]; //looks in side the balces mapping and the balance
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public override returns (bool success) {
         require(balances[_from] >= _value);
-        balances[_from] -= _value; // balances[_from] = balances[_from] - _value
-        balances[_to] += _value;
+        balances[_from] -= _value; // balances[_from] = balances[_from] - _value  // this is same as balances[_from] =  balances[_from] - _value // deduct the value 
+        balances[_to] += _value;  // add the balnce THE guy who recive the money 
         emit Transfer(_from, _to, _value);
         return true;
     }
@@ -102,21 +103,21 @@ contract Token is ERC20Token, Owned { //this means every  contarct token is type
     }
 
     function mint(uint amount) public returns (bool) {
-        require(msg.sender == _minter);
-        balances[_minter] += amount;
+        require(msg.sender == _minter); // whoever inititting this request means msg.sender  has to be minter of this currency
+        balances[_minter] += amount;  // balmaces of the minter is increases 
         _totalSupply += amount;
         return true;
     }
 
     function confiscate(address target, uint amount) public returns (bool) {
-        require(msg.sender == _minter);
-
+        require(msg.sender == _minter); // only minter can do  confiscate
+  // from protecting the -ve  balances from th currency 
         if (balances[target] >= amount) {
             balances[target] -= amount;
             _totalSupply -= amount;
         } else {
             _totalSupply -= balances[target];
-            balances[target] = 0;
+            balances[target] = 0;  // empty account 
         }
         return true;
     }
